aadf00ab8e58e825e1ad1098eb242fc1a0e96170
INovomiast2 <idimnovdie1602@protonmail.com>
2024-01-22 22:54:24 +0100

    Finish tomorrow

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,32 @@
 import SidebarMenu from "@/components/SidebarMenu";
 import Image from "next/image";
+import { IoMdHome } from "react-icons/io";
 import QRCode from "react-qr-code";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex">
       <aside className="h-[100%]">
         <SidebarMenu />
       </aside>
-      <main>
-        
+      <main className="ml-5">
+        <div className="pl-2 mt-5 bg-gray-800 rounded-lg w-80">
+          <h2 className="font-bold">ASISTENTES:</h2>
+          <div className="grid grid-cols-2 row-span-2">
+            <div>
+              <p>Invitados:</p>
+              <span>0</span>
+            </div>
+            <div>
+              <p>Asistentes:</p>
+              <span>0</span>
+            </div>
+          </div>
+        </div>
       </main>
       <footer>
 
       </footer>
-    </>
+    </div>
   );
 }
--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { IoMdHome, IoMdMusicalNote, IoMdPeople } from "react-icons/io";
+import { IoTicket } from "react-icons/io5";
 import { RiMoneyEuroCircleLine } from "react-icons/ri";
 import Link from "next/link";
 
@@ -25,9 +26,15 @@ const SidebarMenu = () => {
         </Link>
       </li>
       <li>
-        <Link href={'/token'}>
+        <Link href={'/tokens'}>
           <RiMoneyEuroCircleLine size={20} /> 
           Tokens
+        </Link>
+      </li>
+      <li>
+        <Link href={'/tickets'}>
+          <IoTicket size={20} /> 
+          Tickets
         </Link>
       </li>
     </ul>
