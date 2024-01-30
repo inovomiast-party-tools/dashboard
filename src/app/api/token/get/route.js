54cdabe169d06603b1f8bcf21305a48fcb55e749
Iván Dimitri Novomiast Dieguez <idimnovdie1602@protonmail.com>
2024-01-30 14:40:26 +0000

    Fixed - Deploy to Production

--- src/app/api/token/get/route.js
+++
@@ -1,7 +0,0 @@
-import Auth from '@/lib/auth';
-import { NextResponse } from 'next/server';
-
-export const GET = () => {
-    const token = Auth.generateToken();
-    return NextResponse.json({ token: token});
-}
